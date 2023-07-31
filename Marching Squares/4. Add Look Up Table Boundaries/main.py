cb48dafa0418b6e47cedcae94c970f629b67162b
Alex Donnelly <alexandercassius@hotmail.com>
2023-07-30 20:03:38 -0400

    added approximation examples

--- Marching Squares/4. Add Look Up Table Boundaries/main.py
+++ Marching Squares/4. Add Look Up Table Boundaries/main.py
@@ -1,50 +1,82 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Main function, entry point of program.
 def main():
-    # Create a grid of x, y points using linspace and meshgrid
-    x = np.linspace(-3, 3, 400)
-    y = np.linspace(-3, 3, 400)
-    X, Y = np.meshgrid(x, y)
-
-    # Compute the value of the distance_from_circle function at each point
-    Z = distance_from_circle_boundary(X, Y)
-
-    # Create a contour plot to visualize the circle (where the distance to the circle's radius is zero)
+    edges = generate_contour_edges(distance_from_circle_boundary)
     plt.figure(figsize=(6,6))
-    contour = plt.contour(X, Y, Z, levels=[0], colors='b')
+    for edge in edges:
+        plt.plot([edge.p0.x, edge.p1.x], [edge.p0.y, edge.p1.y], 'b-')
 
-    # Iterate through whole coordinates in the range [-3, 3] for both x and y
-    for i in range(-3, 4):
-        for j in range(-3, 4):
-            # Calculate the distance from the point to the circle's boundary
-            dist = distance_from_circle_boundary(i, j)
-            # If the distance is positive, the point is outside the circle, and it's plotted with a red dot
-            if dist > 0:
-                plt.plot(i, j, 'ro')
-            # If the distance is negative, the point is inside the circle, and it's plotted with a blue dot
-            elif dist < 0:
-                plt.plot(i, j, 'bo')
-            # Annotate each point with the computed distance, rounded to two decimal places
-            plt.text(i, j, f"{dist:.2f}", fontsize=8, ha='right')
-
-    # Add labels and grid to the plot for better visualization
-    plt.title('Display Distance Function Results')
+    plt.title('Marching Cubes Approximation of Circle')
     plt.xlabel('x')
     plt.ylabel('y')
+    plt.xlim(-3, 3)
+    plt.ylim(-3, 3)
     plt.grid(True)
-
-    # Display the plot
     plt.show()
 
-# Function to calculate the distance from a point (x, y) to a circle with radius 2.5 centered at the origin
+# Generates contour edges for a given function f using the Marching Cubes algorithm
+def generate_contour_edges(f, xmin=-3, xmax=3, ymin=-3, ymax=3):
+    edges = []
+    for x in range(xmin, xmax):
+        for y in range(ymin, ymax):
+            edges.extend(process_single_grid_cell(f, x, y))
+    return edges
+
+# Calculates the distance from a point (x, y) to a circle with radius 2.5 centered at the origin
 def distance_from_circle_boundary(x, y):
-    # Calculate the Euclidean distance from the origin to the point (x, y)
     distance_from_origin = np.sqrt(x**2 + y**2)
-    
-    # Return the difference between the circle's radius and the distance from the origin
     return 2.5 - distance_from_origin
 
-# Entry point of the program: call the main function if the script is run as the main program
+# Class representing an edge with start and end points
+class Edge:
+    def __init__(self, p0, p1):
+        self.p0 = p0
+        self.p1 = p1
+
+    def swap(self, condition):
+        return Edge(self.p1, self.p0) if condition else self
+
+# Class representing a 2D vector with coordinates (x, y)
+class V2:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+# Processes a single grid cell and returns edges that approximate the boundary for that cell
+def process_single_grid_cell(f, x, y):
+    x0y0 = f(x + 0.0, y + 0.0)
+    x0y1 = f(x + 0.0, y + 1.0)
+    x1y0 = f(x + 1.0, y + 0.0)
+    x1y1 = f(x + 1.0, y + 1.0)
+    case = ((1 if x0y0 > 0 else 0) +
+            (2 if x0y1 > 0 else 0) +
+            (4 if x1y0 > 0 else 0) +
+            (8 if x1y1 > 0 else 0))
+
+    if case == 0 or case == 15:
+        return []
+    if case == 1 or case == 14:
+        return [Edge(V2(x, y+0.5), V2(x+0.5, y)).swap(case == 14)]
+    if case == 2 or case == 13:
+        return [Edge(V2(x + 0.5, y + 1), V2(x + 0, y + 0.5)).swap(case == 13)]
+    if case == 4 or case == 11:
+        return [Edge(V2(x + 1, y + 0.5), V2(x + 0.5, y + 0)).swap(case == 11)]
+    if case == 8 or case == 7:
+        return [Edge(V2(x+0.5, y+1), V2(x+1, y+0.5)).swap(case == 7)]
+    if case == 3 or case == 12:
+        return [Edge(V2(x+0.5, y+0), V2(x+0.5, y+1)).swap(case == 12)]
+    if case == 5 or case == 10:
+        return [Edge(V2(x+0, y+0.5), V2(x+1, y+0.5)).swap(case == 10)]
+    if case == 6:
+        return [Edge(V2(x+1, y+0.5), V2(x+0.5, y+0)),
+                Edge(V2(x+0, y+0.5), V2(x+0.5, y+1))]
+    if case == 9:
+        return [Edge(V2(x + 0.5, y), V2(x + 0, y + 0.5)),
+                Edge(V2(x + 0.5, y + 1), V2(x + 1, y + 0.5))]
+
+    assert False, "All cases exhausted"
+
 if __name__ == "__main__":
     main()
